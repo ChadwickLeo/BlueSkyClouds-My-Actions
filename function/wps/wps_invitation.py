eb8c3c02a5d9659ae06805492f5b4db7fca21e28
ChadwickLeo <34240557+ChadwickLeo@users.noreply.github.com>
2022-03-27 10:21:39 +0800

    Update wps_invitation.py

--- function/wps/wps_invitation.py
+++ function/wps/wps_invitation.py
@@ -291,6 +291,7 @@ def docer_webpage_clockin(sid: str):
     docer_url = 'https://zt.wps.cn/2018/docer_check_in/api/checkin_today'
     r = s.post(docer_url, headers={'sid': sid})
     print("签到返回"+str(r))
+    return 0
     if len(r.history) != 0:
         if r.history[0].status_code == 302:
             sio.write("签到失败: 用户sid错误, 请重新输入\n\n")
